1b15a4c133817a7bd2b5f18287ce71d429643dac
arielbaron <arielbar80@gmail.com>
2025-08-16 12:48:18 +0300

    baseTWOMODS

--- main.py
+++ main.py
@@ -38,22 +38,25 @@ def monitor_quit(vlc_proc):
 def handle_playback(vlc_proc):
     threading.Thread(target=monitor_quit, args=(vlc_proc,), daemon=True).start()
 
-def play(*args):
-    if not args:
-        print("No query provided.")
-        return 1
-    query = " ".join(args)
-    url = get_audio_url(query)
-    if not url:
-        return 1
-    vlc_proc = start_vlc(url)
-    handle_playback(vlc_proc)
-
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        play(*sys.argv[1:])
-    else:
+def play(is_cli,*args):
+    if not is_cli:
+        query = " ".join(args)
+        url = get_audio_url(query)
+        vlc_proc = start_vlc(url)
+        handle_playback(vlc_proc)
+        if not url:
+            return 1
+    if is_cli:
         print(Fore.CYAN + Style.BRIGHT + "♫ YTMCLI - YouTube Music CLI")
         print(Fore.MAGENTA + "----------------------------------")
         query = input(Fore.YELLOW + Style.BRIGHT + "♫ Enter song > " + Style.RESET_ALL)
-        play(query)
+        url = get_audio_url(query)
+        vlc_proc = start_vlc(url)
+        handle_playback(vlc_proc)
+        if not url:
+            return 1
+
+if __name__ == "__main__":
+    # CLI prompt logic is kept outside the play function so play() can be used directly in scripts or automation without user interaction.
+    is_cli = len(sys.argv) == 1
+    play(is_cli,*sys.argv[1:])
